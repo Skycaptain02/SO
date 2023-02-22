963c2e6113fcd9bca5c60f23aa64cc3828ee0649
TridelTaxis <matteocarrozzino001@gmail.com>
2023-02-22 10:58:36 +0100

    s

--- Esercizi/Es_matte/Es_1.c
+++ Esercizi/Es_matte/Es_1.c
@@ -5,10 +5,14 @@ int my_function();
 int main(){
     int c = my_function();
     printf("%d", c);
-    int v[10];
     //printf("%d",length(v));
     //int d[-1];
     printf("%ld", sizeof(v)/sizeof(v[0]));
+    char v_2[] = "Ciao";
+    int v[10] = {3, -1, 4};
+    for(int i = 0; i < 10; i++){
+        prinf("%d",v[i]);
+    }
 }
 
 int my_function(){
