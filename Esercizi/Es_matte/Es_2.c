8c14457d264411276e2da0c84fc982ce0d567f59
TridelTaxis <matteocarrozzino001@gmail.com>
2023-02-24 11:00:17 +0100

    d

--- Esercizi/Es_matte/Es_2.c
+++ Esercizi/Es_matte/Es_2.c
@@ -6,7 +6,7 @@ int main(){
     int s[7];
     for(int i = 0; i < 7; i++){
         printf("Inserisci \n");
-        scanf("%i \n", &s[i]);
+        scanf("%i", &s[i]);
     }
     for(int i = 0; i < 7; i++ ){
         printf("%d \n",s[i]);
