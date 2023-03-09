098086519c30cf66f202d652a0059accb5f35b16
TridelTaxis <matteocarrozzino001@gmail.com>
2023-03-09 16:22:27 +0100

    d

---
+++ Esercizi/Es_matte/Nuova cartella/Es_3.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(char * argv[]){
+    char * string;
+    int n_kids = strtol(argv[1],&string,10);
+    int n_writes = strtol(argv[2],&string,10);
+}
